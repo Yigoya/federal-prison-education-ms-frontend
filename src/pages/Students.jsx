03a8611fa8a8d31e68e69a4b7fb903574b7f2132
fitiha <fitihanegst1995@gmail.com>
2024-03-06 23:01:39 +0300

    installed some packages and worked the Students.js section

--- src/pages/Students.jsx
+++ src/pages/Students.jsx
@@ -26,27 +26,37 @@ const rows = [
   { id: 7, department: 'Software Engineering', batch: '2024', section: 'E', email: 'someone@smt.com', phone: '0901010101', lastName: 'Clifford', firstName: 'Ferrara', age: 44, image: 'https://images.unsplash.com/photo-1513956589380-bad6acb9b9d4?q=80&w=1536&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'  },
   { id: 8, department: 'Software Engineering', batch: '2024', section: 'E', email: 'someone@smt.com', phone: '0901010101', lastName: 'Frances', firstName: 'Rossini', age: 36, image: 'https://images.unsplash.com/photo-1513956589380-bad6acb9b9d4?q=80&w=1536&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'  },
   { id: 9, department: 'Software Engineering', batch: '2024', section: 'E', email: 'someone@smt.com', phone: '0901010101', lastName: 'Roxie', firstName: 'Harvey', age: 65, image: 'https://images.unsplash.com/photo-1513956589380-bad6acb9b9d4?q=80&w=1536&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'  },
+  { id: 1, department: 'Software Engineering', batch: '2024', section: 'E', email: 'someone@smt.com', phone: '0901010101', lastName: 'Snow', firstName: 'Jon', age: 35, image: "https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+  { id: 2, department: 'Software Engineering', batch: '2024', section: 'E', email: 'someone@smt.com', phone: '0901010101', lastName: 'Lannister', firstName: 'Cersei', age: 42, image: 'https://images.unsplash.com/photo-1604883781269-d121d9ad20f6?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+  { id: 3, department: 'Software Engineering', batch: '2024', section: 'E', email: 'someone@smt.com', phone: '0901010101', lastName: 'Lannister', firstName: 'Jaime', age: 45, image: 'https://images.unsplash.com/photo-1513956589380-bad6acb9b9d4?q=80&w=1536&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+  { id: 4, department: 'Software Engineering', batch: '2024', section: 'E', email: 'someone@smt.com', phone: '0901010101', lastName: 'Stark', firstName: 'Arya', age: 16, image: 'https://images.unsplash.com/photo-1593132289185-e439d9c99c73?q=80&w=1889&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+  { id: 5, department: 'Software Engineering', batch: '2024', section: 'E', email: 'someone@smt.com', phone: '0901010101', lastName: 'Targaryen', firstName: 'Daenerys', age: null, image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+  { id: 6, department: 'Software Engineering', batch: '2024', section: 'E', email: 'someone@smt.com', phone: '0901010101', lastName: 'Melisandre', firstName: null, age: 150, image: 'https://images.unsplash.com/photo-1513956589380-bad6acb9b9d4?q=80&w=1536&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+  { id: 7, department: 'Software Engineering', batch: '2024', section: 'E', email: 'someone@smt.com', phone: '0901010101', lastName: 'Clifford', firstName: 'Ferrara', age: 44, image: 'https://images.unsplash.com/photo-1513956589380-bad6acb9b9d4?q=80&w=1536&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'  },
+  { id: 8, department: 'Software Engineering', batch: '2024', section: 'E', email: 'someone@smt.com', phone: '0901010101', lastName: 'Frances', firstName: 'Rossini', age: 36, image: 'https://images.unsplash.com/photo-1513956589380-bad6acb9b9d4?q=80&w=1536&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'  },
+  { id: 9, department: 'Software Engineering', batch: '2024', section: 'E', email: 'someone@smt.com', phone: '0901010101', lastName: 'Roxie', firstName: 'Harvey', age: 65, image: 'https://images.unsplash.com/photo-1513956589380-bad6acb9b9d4?q=80&w=1536&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'  },
 ];
 
 export default function Students() {
-  const [selectedRow, setSelectedRow] = useState(null);
+  const [selectedRow, setSelectedRow] = useState({id: 0,lastName: 'Y', firstName: 'X', image: 'https://img.freepik.com/premium-vector/user-vector-icon-outline-style-isolated-white-background-mobile-app-website-logotype-ui-human-symbol-man-silhouette-social-member-sign-personal-profile-sign-10-eps_824631-3088.jpg?w=740'});
 
   const handleRowClick = (params) => {
     setSelectedRow(params.row);
   };
 
   return (
-    <div className='flex'>
-      <div className='m-1 bg-stone-200 w-3/5 height-100% rounded-2xl'>
+    <div className='flex mt-5'>
+      <div className='m-1 bg-stone-200 w-3/5 h-full rounded-2xl'>
         <DataGrid
           rows={rows}
           columns={columns}
+          autoHeight={true}
           initialState={{
             pagination: {
-              paginationModel: { page: 0, pageSize: 50 },
+              paginationModel: { page: 0, pageSize: 10 },
             },
           }}
-          pageSizeOptions={[50, 100]}
+          pageSizeOptions={[5, 10]}
           onRowClick={handleRowClick}
         />
       </div>
@@ -59,7 +69,7 @@ export default function Students() {
           <div className='flex flex-col place-items-center'>
             <img src={selectedRow.image} alt="Profile" className='h-44 w-44 mb-4 rounded-full'/>
             <h1 className='text-xl font-semibold'>{selectedRow.firstName} {selectedRow.lastName}</h1>
-            <h2 >{selectedRow.id}</h2>
+            <h2 >FPS {selectedRow.id}</h2>
           </div>
 
           <div>
